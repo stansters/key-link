9a017fb0d4167d9e700952694a7e31f8408685ff
stansters <stanleygeorgebond@gmail.com>
2022-02-12 17:49:48 +0000

    fuck u

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const { Client, Collection } = require('discord.js');
+const { token } = require('./config.json');
+
+const intents = new Discord.Intents(4609)
+const client = new Client({ intents });
+
+client.once('ready', c => {
+    client.user.setActivity(`hanime.tv`, { type: 'WATCHING' });
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+
+client.on('message', (message) => {
+    if(message.content.includes('key')) {
+        message.reply('https://tr1v5.000webhostapp.com/key.html');
+    }
+});
+
+
+
+
+
+client.login(token);
